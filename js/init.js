83ba9bccb304e1ecf335af7e7a42c00f99827964
alonsoaz <alonsoaz@gmail.com>
2019-08-31 23:05:50 -0500

    second try

---
+++ js/init.js
@@ -0,0 +1,14 @@
+let ENTREVISTADOS = [{ nombre: "Pablo", img: "../img/percy.jpg" }];
+
+function init() {
+  let marco = document.createElement("article");
+  marco.classList.add("marco");
+  let img = document.createElement("ïmg");
+  img.src = ENTREVISTADOS[0].img;
+  img.style.width = "100%";
+  marco.appendChild(img);
+  let videos = document.getElementsByClassName("videos");
+  videos[0].appendChild(marco);
+}
+
+window.onload = init;
